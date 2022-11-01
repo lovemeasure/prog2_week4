303c99c9fa3b5d1790124b3ad974a1f4992bb49d
keito <ik.lovemeasure@gmail.com>
2022-11-01 09:41:40 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import jp.ac.uryukyu.ie.e225746.*;
+
+public class Main {
+    public static void main(String[] args){
+            System.out.println("main method runs");
+
+            Chatbot bot1 = new Chatbot();
+    }
+}
+//演習2_3までやった
---
+++ src/jp/ac/uryukyu/ie/e225746/Chatbot.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e225746;
+
+public class Chatbot {
+    public Chatbot(){
+        System.out.println("チャットボットの原型を作成します。");
+    }
+}
+
